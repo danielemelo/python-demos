7ae1b367ca3d680497b57d29c217402a8b241b3e
Daniele Aparecida <dani.aparecida.ms@gmail.com>
2018-01-15 15:11:44 -0300

    Update sort_list.py

--- sort_list.py
+++ sort_list.py
@@ -1,14 +1,14 @@
 # Sort alphabetically a list of names
 def sort_list():
   while True:
-    changed = False
+    exchanges = False
     for i in range(len(names) - 1):
       if (names[i] > names[i + 1]):
         aux = names[i]
         names[i] = names[i + 1]
         names[i + 1] = aux
-        changed = True
-    if not changed: break
+        exchanges = True
+    if not exchanges: break
 
 names = []
 print "Enter the names. Enter '####' to finish."
